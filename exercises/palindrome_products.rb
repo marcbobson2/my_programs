8c8a28807ee88853c3961b21a2e46d9e40cfdb04
marc davis <marcbobson@gmail.com>
2017-08-09 23:12:16 +0000

    pal

--- exercises/palindrome_products.rb
+++ exercises/palindrome_products.rb
@@ -1,50 +1,31 @@
-class Palindrome
-  def initialize
-    @value = nil
-  end
-
-  def value
-    @value
-  end
-
-  def value=(pal)
-    @value = pal
-  end
-
-  def factors
-    @factors
-  end
-
-  def factors=(list_of_factors)
-    @factors = list_of_factors
-  end
-end
-
-class Largest < Palindrome
-end
-
-class Smallest < Palindrome
-end
 
 
 class Palindromes
   attr_reader :largest, :smallest
+  Palindrome = Struct.new(:value, :factors)
 
   def initialize(factor_hash)
     @max_factor = factor_hash[:max_factor]
-    factor_hash[:min_factor] != nil ? @min_factor = factor_hash[:min_factor] : @min_factor = 1
-    @largest = Largest.new
-    @smallest = Smallest.new
+    @min_factor = factor_hash[:min_factor] || 1
+    @factor_combinations = Hash.new { |hash, key| hash[key] = [] }
+    @palindrome = Palindrome.new
   end
 
   def generate
-    factor_combinations = (@min_factor..@max_factor).to_a.repeated_combination(2).to_a
-    products = factor_combinations.map { |factors| (factors[0] * factors[1]).to_s }
-    palindrome_products = products.select { |product| product == product.reverse }.uniq.map(&:to_i)
-    @largest.value = palindrome_products.max
-    @largest.factors = extract_factors(products, factor_combinations, @largest.value.to_s)
-    @smallest.value = palindrome_products.min
-    @smallest.factors = extract_factors(products, factor_combinations, @smallest.value.to_s)
+
+    (@min_factor..@max_factor).to_a.repeated_combination(2) do |x, y|
+        @factor_combinations[x * y] << [x, y] if is_palindrome?(x, y)
+    end
+  end
+
+  def is_palindrome?(x, y)
+    (x * y).to_s == (x * y).to_s.reverse
+  end
+
+  def largest
+
+    @factor_combinations.max[0]
+    palindrome = Palindrome.new(@factor_combinations.max[0], @factor_combinations.max[1])
   end
 
   def extract_factors(products, factor_combinations, palindrome)
@@ -57,3 +38,8 @@ class Palindromes
 
 end
 
+palindromes = Palindromes.new(max_factor: 999, min_factor: 1)
+palindromes.generate
+largest = palindromes.largest
+p largest.value
+p largest.factors
